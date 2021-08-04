8ee7f943888cdc3865883abaf134b3855a8cc986
Alan Matheus <56325167+alanmrock@users.noreply.github.com>
2021-08-04 14:55:57 -0300

    Update 1.sql

--- exercicios-2/1.sql
+++ exercicios-2/1.sql
@@ -1,1 +1,14 @@
 
+select employees.first_name, employees.last_name, 
+salaries.salary, salaries.from_date, salaries.to_date
+ from employees
+inner join salaries 
+on employees.emp_no = salaries.emp_no 
+order by from_date asc;
+
+select employees.first_name, employees.last_name,
+salaries.salary, salaries.from_date, salaries.to_date
+ from employees
+inner join salaries 
+on employees.emp_no = salaries.emp_no 
+where employees.emp_no = 10006;
