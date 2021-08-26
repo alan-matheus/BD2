f6ccd9de1e9e0582173b71d5529bcb7e37b294e1
Alan Matheus <56325167+alanmrock@users.noreply.github.com>
2021-08-26 13:14:37 -0300

    Create 14.sql

---
+++ exercicios-3/14.sql
@@ -0,0 +1,6 @@
+select  country.Name as 'País', count(countrylanguage.Language) as 'Idiomas'
+from countrylanguage 
+inner join country 
+on country.Code = countrylanguage.CountryCode
+group by country.Name
+having count(countrylanguage.Language)>10;
