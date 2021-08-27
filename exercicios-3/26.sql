84420e08a53841caeccb65aebdf16e128b51565e
Alan Matheus <56325167+alanmrock@users.noreply.github.com>
2021-08-26 21:35:07 -0300

    Create 26.sql

---
+++ exercicios-3/26.sql
@@ -0,0 +1,8 @@
+select * from film;
+select actor.actor_id as 'id ator/atriz', actor.first_name as 'nome',
+ actor.last_name as 'sobrenome', count(film.title) as 'filmes em que atuou'
+from actor inner join film_actor
+on actor.actor_id = film_actor.actor_id
+inner join film
+on film.film_id = film_actor.film_id
+group by actor.actor_id;
