7465cb9439c1d9a8c13d683ff35cbe03a009047d
Alan Matheus <56325167+alanmrock@users.noreply.github.com>
2021-08-26 21:34:04 -0300

    Create 23.sql

---
+++ exercicios-3/23.sql
@@ -0,0 +1,7 @@
+select c.customer_id as 'id do cliente', c.first_name as 'Nome',
+c.last_name as 'Sobrenome',
+sum(p.amount) as 'Total de pagamentos realizados'
+from payment p inner join customer c
+on p.customer_id = c.customer_id
+group by c.customer_id
+having sum(p.amount)>100;
