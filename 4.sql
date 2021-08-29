73dee2a8bdd17582beb7edb3dd86e3004b16a9e6
Alan Matheus <56325167+alanmrock@users.noreply.github.com>
2021-08-29 02:19:52 -0300

    Delete 4.sql

--- 4.sql
+++
@@ -1,8 +0,0 @@
- SELECT film_id, title, rental_duration, rental_rate, description
-  FROM film
-  WHERE exists
-    (SELECT film_id
-       FROM film
-       WHERE rental_duration = 6
-       OR rental_rate = 4.99
-       );
